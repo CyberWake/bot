28d479ecd901de5d67c1046c36fe2ccfd01bc633
CyberWake <ritikfbd@gmail.com>
2024-09-14 11:31:50 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2503456022"
+    serviceWorkerVersion: "2644792109"
   }
 });
